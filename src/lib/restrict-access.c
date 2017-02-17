8577f076602675b8aa12354edc0594543da79fdc
Jin Hsieh <jin.hsieh@canonical.com>
2017-02-17 15:37:26 +0800

    Optional feature: --enable-snap added

--- src/lib/restrict-access.c
+++ src/lib/restrict-access.c
@@ -224,7 +224,7 @@ static void fix_groups_list(const struct restrict_access_settings *set,
 		gid_list = gid_list2;
 	}
 
-#if ENABLE_SNAP
+#if USE_SNAP
 #else
 	if (setgroups(gid_count, gid_list) < 0) {
 		if (errno == EINVAL) {
@@ -349,7 +349,7 @@ void restrict_access(const struct restrict_access_settings *set,
 	}
 
 	/* verify that we actually dropped the privileges */
-#if ENABLE_SNAP
+#if USE_SNAP
     disallow_root = 0;
 #endif
 	if ((target_uid != (uid_t)-1 && target_uid != 0) || disallow_root) {
